3d1791f596fc394b4e1b55a498c3c910377d95bd
HANA <131706436+saadihana@users.noreply.github.com>
2025-06-02 23:29:58 +0200

    Add files via upload
    adding the pages

---
+++ hackathon/tailwind.config.js
@@ -0,0 +1,41 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+
+    extend: {
+      colors: {
+       // "custom-light": "#F7FAFF",
+       // "pur-dark": "#1A0132",
+          "main-bg" : "#1A0132",
+          "secondary" : "#D7AEFF"
+      },
+    },
+
+  },
+  plugins: [addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
